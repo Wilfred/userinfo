a84cb568e16d9987f8dcffe2aee62bf9ae12a4d9
Wilfred Hughes <me@wilfred.me.uk>
2017-02-05 14:39:55 +0000

    Write an example file

---
+++ examples/main.rs
@@ -0,0 +1,10 @@
+extern crate userinfo;
+
+fn main() {
+    let uid = userinfo::current_user_id();
+    println!("Your UID is {}", uid);
+    println!("Your username is {:?}", userinfo::login_name(uid));
+    println!("Your full name is {:?}", userinfo::user_full_name(uid));
+    println!("Your home directory is {:?}", userinfo::user_home_directory(uid));
+    assert!(false);
+}
