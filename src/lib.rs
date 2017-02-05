a0c06f8b83de692f42b4f8e4f087332198c5c43b
Wilfred Hughes <me@wilfred.me.uk>
2017-02-05 15:12:58 +0000

    Conditionally compile the windows implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,19 @@
 #[cfg(unix)]
 extern crate libc;
 
+#[cfg(windows)]
+extern crate winapi;
+#[cfg(windows)]
+extern crate advapi32;
+
 #[cfg(unix)]
 mod unix_impl;
+
+#[cfg(windows)]
+mod windows_impl;
 
 #[cfg(unix)]
 pub use unix_impl::*;
 
-
+#[cfg(windows)]
+pub use windows_impl::*;
