cc268b4aaebde137d2a99f7812af8aabcb2906a9
Wilfred Hughes <me@wilfred.me.uk>
2017-02-05 11:54:36 +0000

    Full name too

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,8 @@ pub fn current_username() -> String {
     username(current_user_id()).unwrap()
 }
 
-/// Return the login name of the user with the UID given.
+/// Return the login name of the user with the UID given. For example,
+/// `"jsmith"`.
 ///
 /// If the UID does not exist, or the username is invalid UTF-8,
 /// returns None.
@@ -25,7 +26,30 @@ pub fn username(uid: uid_t) -> Option<String> {
     let name_cstr = unsafe { CStr::from_ptr((*passwd_info).pw_name) };
     match name_cstr.to_str() {
         Ok(name) => Some(name.to_owned()),
-        Err(_) => None
+        Err(_) => None,
+    }
+}
+
+/// Return the login name of the user with the UID given. For example,
+/// `"John Smith"`.
+///
+/// If the UID does not exist, if the full name is not set, or if the
+/// full is invalid UTF-8, returns None.
+pub fn user_full_name(uid: uid_t) -> Option<String> {
+    let passwd_info = unsafe { libc::getpwuid(uid) };
+    if passwd_info.is_null() {
+        return None;
+    }
+    let name_cstr = unsafe { CStr::from_ptr((*passwd_info).pw_gecos) };
+    match name_cstr.to_str() {
+        Ok(name) => {
+            if name.len() > 0 {
+                Some(name.to_owned())
+            } else {
+                None
+            }
+        }
+        Err(_) => None,
     }
 }
 
@@ -37,6 +61,8 @@ mod tests {
     fn test_root_uid() {
         println!("your uid is {}", current_user_id());
         println!("your username is {}", current_username());
+        println!("your full name is {}",
+                 user_full_name(current_user_id()).unwrap());
         assert_eq!(username(0).unwrap(), "root");
     }
 }
