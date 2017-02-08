22ca8d3934e696911881a90e53de8a28fcbea70a
Wilfred Hughes <me@wilfred.me.uk>
2017-02-08 00:20:47 +0000

    Add windows placeholder implementations with the correct calling conventions

--- src/windows_impl.rs
+++ src/windows_impl.rs
@@ -1,22 +1,40 @@
 extern crate winapi;
 extern crate advapi32;
 
+use std::path::PathBuf;
 use winapi::lmcons::UNLEN;
 use winapi::winnt::WCHAR;
 use advapi32::GetUserNameW;
 
+type uid_t = u64;
+type gid_t = u64;
     
-pub fn user_id() -> u64 {
+pub fn current_user_id() -> uid_t {
     12
 }
 
-pub fn user_name() -> String {
+pub fn current_group_id() -> gid_t {
+    12
+}
+
+pub fn login_name(_uid: uid_t) -> Option<String> {
     let mut buf: [WCHAR; (UNLEN + 1) as usize] = [0; (UNLEN + 1) as usize];
     let mut buf_size = UNLEN + 1;
     unsafe {
         GetUserNameW(buf.as_mut_ptr(), &mut buf_size);
     }
-    String::from_utf16(&buf).unwrap()
+    match String::from_utf16(&buf) {
+        Ok(name) => Some(name),
+        Err(_) => None,
+    }
+}
+
+pub fn user_full_name(_uid: uid_t) -> Option<String> {
+    Some("foobar".to_owned())
+}
+
+pub fn user_home_directory(_uid: uid_t) -> Option<PathBuf> {
+    Some(PathBuf::from("c:\\"))
 }
 
 #[cfg(test)]
@@ -25,7 +43,7 @@ mod tests {
 
     #[test]
     fn test_user_id() {
-        println!("your name is {}", user_name());
-        assert_eq!(user_id(), 12);
+        println!("your name is {:?}", login_name(0));
+        assert_eq!(current_user_id(), 12);
     }
 }
